c1e4c22489f5422311bece7c2243dac5ea8463a4
Craig McNamara <cmcnamara87@gmail.cok>
2014-06-28 18:57:40 +1000

    Add collections list page

--- app/scripts/controllers/collections.js
+++ app/scripts/controllers/collections.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('screenshotApp')
-    .controller('CollectionsCtrl', function($scope) {
+    .controller('CollectionsCtrl', function($scope, collections) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
+        $scope.collections = collections;
     });
